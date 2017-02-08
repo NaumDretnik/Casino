678e392b376fa92d5b60d094781968b87875522a
Naum <naumaram@gmail.com>
2017-02-08 19:41:51 +0100

    dodan casino

---
+++ casino.py
@@ -0,0 +1,26 @@
+print("Welcome to the Lucky Number!")
+
+lucky_number = raw_input("Pick a number between 0 and 100,000: ")
+
+while lucky_number == "18765":
+    print("Congratulations! You won an unlimited supply of cocoa!")
+    break
+
+else:
+    print("Two more guesses...")
+    lucky_number = raw_input("Pick a number between 0 and 100,000: ")
+
+    while lucky_number == "18765":
+        print("Congratulations! You won an unlimited supply of cocoa!")
+        break
+
+    else:
+        print("One more guess... ")
+        lucky_number = raw_input("Pick a number between 0 and 100,000: ")
+
+        while lucky_number == "18765":
+            print("Congratulations! You have won an unlimited supply of cocoa!")
+            break
+
+        else:
+            print("Sorry, wrong number... Better luck next time!")
